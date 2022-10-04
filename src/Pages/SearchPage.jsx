ae57e5c2256c3149b605960a9cccd41b6428f894
MarcusHjorth <marcusphr1808@gmail.com>
2022-10-04 15:05:51 +0200

    Searchpage

--- src/Pages/SearchPage.jsx
+++ src/Pages/SearchPage.jsx
@@ -1,10 +1,19 @@
+import AllActivities from "../Components/Activities/AllActivities";
+import SearchBar from "../Components/Search/SearchBar";
+import Title from "../Components/Title";
 import Navigation from "../Templates/Navigation";
 
 const SearchPage = () => {
     return ( 
         <main>
-            Search
+            <div className="p-[20px]">
+                <Title text="Søg" />
 
+                <SearchBar />
+
+                <AllActivities />
+
+            </div>
             <Navigation />
         </main>
      );
