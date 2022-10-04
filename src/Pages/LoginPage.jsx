1c694e5b1bd72f9ad5d2bbeabf76e906489acb2b
MarcusHjorth <marcusphr1808@gmail.com>
2022-10-04 16:21:37 +0200

    Logo

--- src/Pages/LoginPage.jsx
+++ src/Pages/LoginPage.jsx
@@ -1,7 +1,41 @@
+import Baggrund from "../Assets/Baggrund.png"
+
 const LoginPage = () => {
     return ( 
-        <main>
+        <main className="h-screen">
+            <figure className="relative h-full w-full">
+                <img src={Baggrund} alt="" className="h-full w-full object-cover" />
+                <div className="absolute top-[-150px] h-[1000px] rotate-[70deg] w-[550px] bg-Dark bg-opacity-40 overflow-hidden"></div>
             
+                <div className="absolute top-[30vh] p-[40px]">
+                    <h1 className="text-[48px]">Log ind</h1>
+                    <form className="flex flex-col items-center"
+                    onSubmit={handleSubmit(onSubmit)}
+                >
+                    <p> {errors.username?.message} </p>
+                    <input 
+                    type="text" 
+                    placeholder="Enter your email..." 
+                    className="outline-none bg-OffWhite mt-[10px] py-[1rem] font-semibold rounded-full w-full border-LightGray border-2 pl-[15px]"
+                        {...register("username")}
+                    />
+
+                    <p> {errors.password?.message} </p>
+                    <input 
+                    type="password" 
+                    placeholder="Enter your password..." 
+                    className="outline-none bg-OffWhite mt-[10px] py-[1rem] font-semibold rounded-full w-full border-LightGray border-2 pl-[15px]"
+                        {...register("password")}
+                    />
+                    
+                    <input 
+                        type="submit" 
+                        value="LOG IN" 
+                        className="bg-Yellow mt-[10px] py-[1rem] font-semibold rounded-full w-full"/>
+                </form>
+                    dfsf
+                </div>
+            </figure>
         </main>
      );
 }
