32de57adcbe102b8f1166c6a22f923ab999cbcfb
MarcusHjorth <marcusphr1808@gmail.com>
2022-10-05 16:49:09 +0200

    Sign Up For Activity

--- src/Pages/ActivityDetails.jsx
+++ src/Pages/ActivityDetails.jsx
@@ -1,12 +1,15 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { useParams } from "react-router-dom";
 import axios from "axios";
+import { ContextState } from "../Context/Context";
 
 import Navigation from "../Templates/Navigation";
+import AddToActivity from '../Components/ActivityDetails/AddToActivity';
 
 const ActivityDetails = () => {
+    const context = useContext( ContextState )
     const URL = "http://localhost:4000/api/v1/activities/"
-    const [ item, setItem ] = useState()
+    const [ data, setData ] = useState()
     const { id } = useParams()
 
     useEffect(() => {
@@ -14,28 +17,29 @@ const ActivityDetails = () => {
             url: URL + id,
             method: "GET",
         }).then(request => { 
-            setItem(request.data)
+            setData(request.data)
         })
     }, [ id ])
 
-    console.log(item);
+    console.log(data);
     return ( 
-        item ?
+        data ?
         <main className="text-Secondary font-semibold">
             <figure className="relative">
-                <img src={ item.asset.url } alt="" className="w-full h-[60vh] object-cover"/>
-                <button className="absolute bottom-[10%] right-[10%] px-[75px] py-[15px] object-center bg-Primary rounded-[10px] shadow-lg">Tilmeld</button>
+                <img src={ data.asset.url } alt="" className="w-full h-[60vh] object-cover"/>
+                { context.isLoggedIn && 
+                    <AddToActivity id={ data.id}/>
+                }
             </figure>
 
             <div className="p-[20px]">
-                <h1 className="text-24">{ item.name }</h1>
-                <p>{ item.minAge } - { item.maxAge } år</p>
+                <h1 className="text-24">{ data.name }</h1>
+                <p>{ data.minAge } - { data.maxAge } år</p>
                 <br />
-                <p>{ item.weekday } { item.time }</p>
+                <p>{ data.weekday } { data.time }</p>
                 <br />
-                <p>{ item.description }</p>
+                <p>{ data.description }</p>
             </div>
-
 
             <Navigation />
         </main> : <p>Loading..</p>
